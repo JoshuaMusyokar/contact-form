3c3a05e8679f580ef95239c0ed1a4c039a969a04
JoshuaMusyoka <joshuamusyokar@gmail.com>
2023-04-30 13:11:29 +0300

    fourth commit

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.post("/send_email", (req, res) => {
   const email = req.body.email;
   const subject = req.body.subject;
   const message = req.body.text;
+
+  console.log(req);
   const transporter = nodemailer.createTransport({
     // host: 'smtp.gmail.com',
     // port: 587,
@@ -23,14 +25,23 @@ app.post("/send_email", (req, res) => {
       pass: "oqybajxoegwjwljr",
     },
   });
-  transporter.sendMail({
-    to: email,
-    from: "joshuamusyokar@gmail.com",
-    subject: subject,
-    html: `<h1>thanks for reaching me, ${from}☺</h1><h3>${message}</h3>`,
-  });
+  transporter.sendMail(
+    {
+      to: email,
+      from: "joshuamusyokar@gmail.com",
+      subject: subject,
+      html: `<h1>thanks for reaching me, ${from}☺</h1><h3>${message}</h3>`,
+    },
+    function (err, data) {
+      if (err) {
+        alert(`sorry ${from} problem occured when reaching`);
+      } else {
+        alert(`Hey ${from}Email sent successfully`);
+      }
+    }
+  );
 
-  res.send(alert("email sent successfully✅")).redirect("/");
+  res.redirect("http://myportofolio-joshuamusyoka.netlify.app");
 });
 
 app.listen(process.env.PORT || 3000);
